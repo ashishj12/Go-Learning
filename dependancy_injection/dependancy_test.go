9afb3fa9cd9a7a223fd47cce49143b01073b88d6
ashishj12 <jangidashish38@gmail.com>
2025-09-18 14:00:40 +0530

    dependancy injection

---
+++ dependancy_injection/dependancy_test.go
@@ -0,0 +1,20 @@
+package main
+
+// import (
+// 	"bytes"
+// 	"testing"
+// )
+
+// func TestGreet(t *testing.T) {
+
+// 	buffer := bytes.Buffer{}
+// 	Greet(&buffer, "golang")
+// 	got := buffer.String()
+// 	want := "hello, golang"
+
+// 	if got != want {
+// 		t.Errorf("got %s want %s", got, want)
+// 	}
+
+// }
+
